1762254a908dedd268a6fde0fc106155436d3e99
GoodGuyChris <150680867+ChrisScal@users.noreply.github.com>
2023-11-25 17:56:22 +0200

    Update main.py
    Balame kai screen

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
 import pygame as pygame
+import sys
+
+pygame.init()#kanei initialize to module (einai aparaitito)
+screen = pygame.display.set_mode((400,500))#window
+clock = pygame.time.Clock()
+
+#main game
+while True:
+    for event in pygame.event.get():
+       if event.type == pygame.QUIT:
+           pygame.quit()#kleinei to game
+           sys.exit()
+    screen.fill((175,215,70))
+
+    #edw pane ta graphic elements
+    pygame.display.update()
+    clock.tick(60)#framerate --> 60fps locked :)
