d0e694dd2dd6d26cddcdd0c0b152883beb78a7d4
Javebes <151533116+Javebes@users.noreply.github.com>
2023-12-15 19:14:31 +0200

    Add files via upload
    Allo ena epityxhmeno update. Meiwsame tis entoles hxou, cause merikes apo autes htan axrhstes. Opote kaname kati san optimization. Also pillarmen theme

--- Sound_snake.py
+++ Sound_snake.py
@@ -1,6 +1,182 @@
 import pygame as pygame
 import sys,random
 from pygame.math import Vector2
+from button import Button
+
+sound = True
+SCREEN = pygame.display.set_mode((800, 800))
+
+BG = pygame.image.load("assets/Background.png")
+pygame.display.set_caption('snake game')
+
+def main_menu():
+    sound = True
+    running = True
+    while running:
+        SCREEN.blit(BG, (0, 0))
+        MENU_MOUSE_POS = pygame.mouse.get_pos()
+
+        MENU_TEXT = get_font(50).render("MAIN MENU", True, "#b68f40")
+        MENU_RECT = MENU_TEXT.get_rect(center=(400, 100))
+
+        PLAY_BUTTON = Button(image=pygame.image.load("assets/Play Rect.png"), pos=(400, 200), 
+                            text_input="PLAY", font=get_font(75), base_color="#d7fcd4", hovering_color="White")
+        OPTIONS_BUTTON = Button(image=pygame.image.load("assets/Options Rect.png"), pos=(400, 400), 
+                            text_input="OPTIONS", font=get_font(75), base_color="#d7fcd4", hovering_color="White")
+        QUIT_BUTTON = Button(image=pygame.image.load("assets/Quit Rect.png"), pos=(400, 600), 
+                            text_input="QUIT", font=get_font(75), base_color="#d7fcd4", hovering_color="White")
+
+        SCREEN.blit(MENU_TEXT, MENU_RECT)
+
+        for button in [PLAY_BUTTON, OPTIONS_BUTTON, QUIT_BUTTON]:
+            button.changeColor(MENU_MOUSE_POS)
+            button.update(SCREEN)
+        
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if PLAY_BUTTON.checkForInput(MENU_MOUSE_POS):
+                    pygame.display.set_caption('snake game')
+                    running = False
+                    pygame.mixer_music.unload()
+                if OPTIONS_BUTTON.checkForInput(MENU_MOUSE_POS):
+                    options()
+                if QUIT_BUTTON.checkForInput(MENU_MOUSE_POS):
+                    pygame.quit()
+                    sys.exit() 
+            #gia convinient logous        
+            if event.type == pygame.KEYDOWN: 
+                if event.key == pygame.K_m:
+                 sound = not sound
+                 if sound == False:
+                    main_game.mute_game_sound()
+                 else:
+                    main_game.open__sd()
+                if event.key == pygame.K_q:
+                  main_game.lower_sd()
+                if event.key == pygame.K_e:
+                    main_game.higher_sd()
+                if event.key == pygame.K_SPACE:
+                    pygame.display.set_caption('snake game')
+                    running = False
+                    pygame.mixer_music.unload()
+        pygame.display.update()
+        
+        
+def options():
+    while True:
+        OPTIONS_MOUSE_POS = pygame.mouse.get_pos()
+
+        SCREEN.blit(BG, (0, 0))
+
+        OPTIONS_TEXT = get_font(20).render("This is the OPTIONS screen.", True, "#b68f40")
+        OPTIONS_RECT = OPTIONS_TEXT.get_rect(center=(400, 160))
+        SCREEN.blit(OPTIONS_TEXT, OPTIONS_RECT)
+
+        OPTIONS_BACK = Button(image=None, pos=(400, 600), 
+                            text_input="BACK", font=get_font(50), base_color="#d7fcd4", hovering_color="White")
+
+        OPTIONS_BACK.changeColor(OPTIONS_MOUSE_POS)
+        OPTIONS_BACK.update(SCREEN)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if OPTIONS_BACK.checkForInput(OPTIONS_MOUSE_POS):
+                    main_menu()
+
+        pygame.display.update()
+
+def get_font(size): # Returns Press-Start-2P in the desired size
+    return pygame.font.Font("assets/font.ttf", size)
+
+
+        
+pygame.init()#kanei initialize to module (einai aparaitito)
+cell_size = 40
+cell_number = 20
+screen = pygame.display.set_mode((cell_number*cell_size, cell_number*cell_size))#window
+clock = pygame.time.Clock()
+apple = pygame.image.load('Graphics/apple.png').convert_alpha()
+game_font = pygame.font.Font(None ,25) #bale font !!!!!!!!
+SCREEN_UPDATE = pygame.USEREVENT
+pygame.time.set_timer(SCREEN_UPDATE,150) #animation timer
+#main game
+
+class MAIN :
+    def __init__(self):
+        self.snake = Snake()
+        self.fruit = Fruit()
+        self.volume= 0.5
+        pygame.mixer.music.set_volume(self.volume)
+        
+    def update(self):
+       self.snake.move_snake() 
+       self.check_collision()
+       self.check_fail()
+       
+    
+    def draw_elements(self):
+        self.draw_grass()
+        self.fruit.draw_fruit()
+        self.snake.draw_snake()
+        self.draw_score()
+        
+        
+    def check_collision(self):
+        if self.fruit.pos == self.snake.body[0]:
+            self.fruit.randomize()
+            #reposition to mhlo
+            self.snake.addblock()
+            #+1 block sto snecko
+            for block in self.snake.body[1:]:
+                if block == self.fruit.pos:
+                    self.fruit.randomize()
+            
+    def check_fail(self):
+        #check an snake ektos screen
+        if not 0 <= self.snake.body[0].x < cell_number or not 0<= self.snake.body[0].y < cell_number:
+            self.game_over()
+            
+        #check kanibalismos
+        for block in self.snake.body[1:]:
+            if block == self.snake.body[0]:
+                self.game_over()
+            
+    def draw_grass(self):
+        grass_color = (167,209,61)#skouro
+        for row in range(cell_number):
+            if row %2 ==0:
+                for col in range(cell_number):
+                    if col % 2 == 0 :
+                        grass_rect = pygame.Rect(col * cell_size,row * cell_size,cell_size,cell_size)
+                        pygame.draw.rect(screen, grass_color, grass_rect)
+            else:
+                for col in range(cell_number):
+                    if col % 2 != 0 :
+                        grass_rect = pygame.Rect(col * cell_size,row * cell_size,cell_size,cell_size)
+                        pygame.draw.rect(screen, grass_color, grass_rect)
+                    
+    def draw_score(self):
+        score_text = str(len(self.snake.body) - 3)
+        score_surface = game_font.render(score_text,True,(56,74,12))
+        score_x= cell_size*cell_number - 60
+        score_y= cell_size*cell_number - 40
+        score_rect = score_surface.get_rect(center = (score_x,score_y))
+        apple_rect = apple.get_rect(midright = (score_rect.left, score_rect.centery))
+        bg_rect = pygame.Rect(apple_rect.left, apple_rect.top ,apple_rect.width + score_rect.width + 6 ,apple_rect.height)
+        
+        pygame.draw.rect(screen,(167,209,61) , bg_rect)
+        screen.blit(score_surface,score_rect)
+        screen.blit(apple,apple_rect)
+        pygame.draw.rect(screen,(56,74,12) , bg_rect,2)
+    
+    def game_over(self):
+        self.snake.reset()
 
 class Snake:
     #To fydi
@@ -121,72 +297,24 @@ class Fruit :
         self.y = random.randint(0,cell_number-1)
         self.pos = Vector2(self.x, self.y)#disdiastatos pinakas syntetagmenwn (bloebei anti gia lista argotera)
         
+pygame.init()#kanei initialize to module (einai aparaitito)
+cell_size = 40
+cell_number = 20
+screen = pygame.display.set_mode((cell_number*cell_size, cell_number*cell_size))#window
+clock = pygame.time.Clock()
+apple = pygame.image.load('Graphics/apple.png').convert_alpha()
+game_font = pygame.font.Font(None ,25) #bale font !!!!!!!!
+SCREEN_UPDATE = pygame.USEREVENT
+pygame.time.set_timer(SCREEN_UPDATE,150) #animation timer
+#main game
 
 class MAIN :
     def __init__(self):
         self.snake = Snake()
         self.fruit = Fruit()
-        volume = 0.5
-        self.volume= volume
+        self.volume= 0.5
         pygame.mixer.music.set_volume(self.volume)
-       
         
-        
-              
-    
-    #giam giam noises, poly satisfying 
-    def play_crunch_sound(self):
-        pygame.mixer.music.load("sound/Apple-bite.mp3") 
-        pygame.mixer.music.play()
-
-    def open_crunch_sd(self):
-        self.play_crunch_sound()
-        pygame.mixer.music.set_volume(self.volume)
-
-    def mute_crunch_sound(self):
-        self.play_crunch_sound()
-        pygame.mixer.music.set_volume(0) 
-    
-    #gia strofes
-    def play_turn_sd(self):
-        pygame.mixer.music.load("sound/turn_2.wav") 
-        pygame.mixer.music.play()
-    
-    def open_turn(self):
-        self.play_turn_sd()
-        pygame.mixer.music.set_volume(self.volume)
-    
-    def mute_turn_sound(self):
-        self.play_turn_sd()
-        pygame.mixer.music.set_volume(0)
-    
-    def open_sd(self):
-        self.open_crunch_sd()
-        self.open_turn()
-
-    def higher_sd(self):
-        self.volume += 0.1
-        if self.volume> 1:
-            self.volume = 1
-        pygame.mixer.music.set_volume(self.volume)
-    
-    def mute_sound(self):
-        self.mute_crunch_sound()
-        self.mute_turn_sound()
-
-    def lower_sd(self):
-        self.volume -= 0.1
-        if self.volume< 0:
-            self.volume = 0
-        pygame.mixer.music.set_volume(self.volume)
-    
-    #to perifhmo pillarmen theme, to exw ws sxolio gia na to energopoihsw otan ftia3ete to menu
-    '''def play_background_sound(self):
-        pygame.mixer.music.load("Pillar_men.mp3.mp3")
-        pygame.mixer.music.play(-1)
-        pygame.mixer.music.set_volume(0.5)'''
-    
-    
     def update(self):
        self.snake.move_snake() 
        self.check_collision()
@@ -198,12 +326,47 @@ class MAIN :
         self.fruit.draw_fruit()
         self.snake.draw_snake()
         self.draw_score()
+
+ #more like pause menu music
+    def play_menu_sd(self):
+        pygame.mixer.music.load("sound/Pillar_men.mp3.mp3") 
+        pygame.mixer.music.play(-1)
+
+    #giam giam noises
+    def play_crunch_sound(self):
+        pygame.mixer.music.load("sound/Apple-bite.mp3") 
+        pygame.mixer.music.play()
+    
+    #gia strofes
+    def play_turn_sd(self):
+        pygame.mixer.music.load("sound/turn_2.wav") 
+        pygame.mixer.music.play()
+
+
+    #kapoia genika gia to sound kounabhs geniko
+    def open__sd(self):
+        pygame.mixer.music.set_volume(self.volume)
+
+    def mute_game_sound(self):
+        pygame.mixer.music.set_volume(0) 
+    
+    def higher_sd(self):
+        self.volume += 0.1
+        if self.volume> 1:
+            self.volume = 1
+        pygame.mixer.music.set_volume(self.volume)
+
+    def lower_sd(self):
+        self.volume -= 0.1
+        if self.volume< 0:
+            self.volume = 0
+        pygame.mixer.music.set_volume(self.volume)
         
         
     def check_collision(self):
         if self.fruit.pos == self.snake.body[0]:
-            self.play_crunch_sound()
             self.fruit.randomize()
+            self.play_crunch_sound()
             #reposition to mhlo
             self.snake.addblock()
             #+1 block sto snecko
@@ -243,6 +406,7 @@ class MAIN :
         score_rect = score_surface.get_rect(center = (score_x,score_y))
         apple_rect = apple.get_rect(midright = (score_rect.left, score_rect.centery))
         bg_rect = pygame.Rect(apple_rect.left, apple_rect.top ,apple_rect.width + score_rect.width + 6 ,apple_rect.height)
+        
         pygame.draw.rect(screen,(167,209,61) , bg_rect)
         screen.blit(score_surface,score_rect)
         screen.blit(apple,apple_rect)
@@ -252,30 +416,17 @@ class MAIN :
         self.snake.reset()
 
 
-pygame.init()#kanei initialize to module (einai aparaitito)
-cell_size = 40
-cell_number = 20
-screen = pygame.display.set_mode((cell_number*cell_size, cell_number*cell_size))#window
-clock = pygame.time.Clock()
-apple = pygame.image.load('Graphics/apple.png').convert_alpha()
-game_font = pygame.font.Font(None ,25)#bale font !!!!!!!!
 
-#ena touch
-open = pygame.image.load('Graphics/open.png').convert_alpha()
-mute = pygame.image.load('Graphics/mute.png').convert_alpha()
-megaphone = open
-
-
-SCREEN_UPDATE = pygame.USEREVENT
-pygame.time.set_timer(SCREEN_UPDATE,150) #animation timer
-pygame.display.set_caption('Snake Game')
-#main game
 
 main_game = MAIN()
-sound = False
 
 while True:
     for event in pygame.event.get():
+       if event.type == pygame.KEYDOWN:
+           if event.key == pygame.K_SPACE:
+                pygame.display.set_caption('pause menu')
+                main_game.play_menu_sd()
+                main_menu()
        if event.type == pygame.QUIT:
            pygame.quit()#kleinei to game
            sys.exit()
@@ -316,11 +467,11 @@ while True:
                     main_game.snake.direction = Vector2(-1,0)
                     main_game.play_turn_sd()
            if event.key == pygame.K_m:
-               sound= not sound
+               sound = not sound
                if sound == False:
-                   main_game.mute_sound()
+                   main_game.mute_game_sound()
                else:
-                   main_game.open_sd()
+                   main_game.open__sd()
            if event.key == pygame.K_q:
                main_game.lower_sd()
            if event.key == pygame.K_e:
@@ -332,3 +483,4 @@ while True:
     #edw pane ta graphic elements
     pygame.display.update()
     clock.tick(60)#framerate --> 60fps locked :)
+#:) ez
